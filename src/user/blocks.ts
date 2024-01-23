a9bd4ac0d65c2d49e5e8f1b759161b05a40fe745
Rebecca <107712113+rebeccali04@users.noreply.github.com>
2024-01-23 16:10:12 -0500

    String errors fixed

--- src/user/blocks.ts
+++ src/user/blocks.ts
@@ -18,16 +18,28 @@ function blockFunction(User) {
             max: 100,
             ttl: 0,
         }),
-        is: function (targetUid, uids) {
+        is: function (targetUid:string, uids) {
             return __awaiter(this, void 0, void 0, function* () {
                 const isArray = Array.isArray(uids);
                 uids = isArray ? uids : [uids];
                 const blocks = yield User.blocks.list(uids);
-                const isBlocked = uids.map((uid, index) => blocks[index].includes(parseInt(targetUid.toString(), 10)));
+                // const isBlocked: boolean[] = uids.map((uid: number, index: number) => (blocks[index] as number[]).includes(parseInt(targetUid.toString(), 10)));
+                const isBlocked = uids.map((uid, index) => {
+                    const blockList = blocks[index];
+                
+                    if (Array.isArray(blockList)) {
+                        // Type guard to ensure blockList is treated as an array
+                        const isUidBlocked = blockList.includes(parseInt(targetUid, 10) || 0);
+                        return isUidBlocked;
+                    } else {
+                        // Handle the case where blockList is undefined or not an array
+                        return false; // or handle it in a way that makes sense for your application
+                    }
+                });                
                 return isArray ? isBlocked : isBlocked[0];
             });
         },
-        can: function (callerUid, blockerUid, blockeeUid, type) {
+        can: function (callerUid:string, blockerUid:string|number, blockeeUid:number, type) {
             return __awaiter(this, void 0, void 0, function* () {
                 // Guests can't block
                 if (blockerUid === 0 || blockeeUid === 0) {
@@ -45,18 +57,29 @@ function blockFunction(User) {
                 if (isBlockeeAdminOrMod && type === 'block') {
                     throw new Error('[[error:cannot-block-privileged]]');
                 }
-                if (parseInt(callerUid.toString(), 10) !== parseInt(blockerUid.toString(), 10) && !isCallerAdminOrMod) {
+                // Changed to account for types of blockerUid
+                let parsedBlockerUid: number;
+                if (typeof blockerUid === 'string'){
+                    parsedBlockerUid = parseInt(blockerUid, 10)
+                }
+                else{
+                    parsedBlockerUid = blockerUid;
+                }
+                
+                if (parseInt(callerUid, 10) !== parsedBlockerUid && !isCallerAdminOrMod) {
                     throw new Error('[[error:no-privileges]]');
                 }
             });
         },
-        list: function (uids) {
+        list: function (uids:string[]) {
             return __awaiter(this, void 0, void 0, function* () {
                 const isArray = Array.isArray(uids);
-                uids = (isArray ? (Array.isArray(uids) ? uids : [uids]) : [uids]);
-                uids = uids.map(uid => parseInt(uid.toString(), 10));
+                const processedUids = (isArray ? (Array.isArray(uids) ? uids : [uids]) : [uids]);
+                // Changed assigned newUids instead of passing it to uids
+                const newUids = processedUids.map(uid => parseInt(uid, 10));
+
                 const cachedData = {};
-                const unCachedUids = User.blocks._cache.getUnCachedKeys(uids, cachedData);
+                const unCachedUids = User.blocks._cache.getUnCachedKeys(newUids, cachedData);
                 if (unCachedUids.length) {
                     const unCachedData = yield db.getSortedSetsMembers(unCachedUids.map(uid => `uid:${uid}:blocked_uids`));
                     unCachedUids.forEach((uid, index) => {
@@ -64,25 +87,25 @@ function blockFunction(User) {
                         User.blocks._cache.set(uid, cachedData[uid]);
                     });
                 }
-                const result = uids.map(uid => cachedData[uid] || []);
+                const result = newUids.map(uid => cachedData[uid] || []);
                 return isArray ? result.slice() : result[0];
             });
         },
-        add: function (targetUid, uid) {
+        add: function (targetUid, uid:string) {
             return __awaiter(this, void 0, void 0, function* () {
                 yield User.blocks.applyChecks('block', targetUid, uid);
                 yield db.sortedSetAdd(`uid:${uid}:blocked_uids`, Date.now(), targetUid);
                 yield User.incrementUserFieldBy(uid, 'blocksCount', 1);
-                User.blocks._cache.del(parseInt(uid.toString(), 10));
+                User.blocks._cache.del(parseInt(uid, 10));
                 plugins.hooks.fire('action:user.blocks.add', { uid: uid, targetUid: targetUid });
             });
         },
-        remove: function (targetUid, uid) {
+        remove: function (targetUid, uid:string) {
             return __awaiter(this, void 0, void 0, function* () {
                 yield User.blocks.applyChecks('unblock', targetUid, uid);
                 yield db.sortedSetRemove(`uid:${uid}:blocked_uids`, targetUid);
                 yield User.decrementUserFieldBy(uid, 'blocksCount', 1);
-                User.blocks._cache.del(parseInt(uid.toString(), 10));
+                User.blocks._cache.del(parseInt(uid, 10));
                 plugins.hooks.fire('action:user.blocks.remove', { uid: uid, targetUid: targetUid });
             });
         },
@@ -117,7 +140,7 @@ function blockFunction(User) {
                 const blocked_uids = yield User.blocks.list(uid);
                 const flatBlockedUids = [].concat(...blocked_uids);
                 const blockedSet = new Set(flatBlockedUids);
-                set = set.filter(item => !blockedSet.has(parseInt(isPlain ? item : (item && item[property]), 10)));
+                set = set.filter((item) => !blockedSet.has(parseInt(isPlain ? item : (item && item[property]), 10)));
                 const data = yield plugins.hooks.fire('filter:user.blocks.filter', { set: set, property: property, uid: uid, blockedSet: blockedSet });
                 return data.set;
             });
